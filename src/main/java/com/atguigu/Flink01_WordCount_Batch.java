2392aa2373918790878ad7a11b88478cbebe59aa
AmbitiondX <ambitiondx@163.com>
2021-11-08 11:15:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/Flink01_WordCount_Batch.java
@@ -0,0 +1,38 @@
+package com.atguigu;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.*;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class Flink01_WordCount_Batch {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> dataSource = env.readTextFile("input/word.txt");
+        FlatMapOperator<String, String> flatMap = dataSource.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String s, Collector<String> collector) throws Exception {
+                String[] split = s.split(" ");
+                for (String s1 : split) {
+                    collector.collect(s1);
+                }
+            }
+        });
+        MapOperator<String, Tuple2<String, Integer>> map = flatMap.map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2 map(String s) throws Exception {
+                return Tuple2.of(s, 1);
+            }
+        });
+
+        UnsortedGrouping<Tuple2<String, Integer>> groupBy = map.groupBy(0);
+
+        AggregateOperator<Tuple2<String, Integer>> sum = groupBy.sum(1);
+
+        sum.print();
+
+
+    }
+}
